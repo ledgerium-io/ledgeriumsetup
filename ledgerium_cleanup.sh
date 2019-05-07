c3fec6c9b546462853bbdb93b815aa71a61c86a5
golashr <rahul.g@ledgerium.net>
2019-05-07 14:56:13 +1000

    script updated

--- ledgerium_cleanup.sh
+++ ledgerium_cleanup.sh
@@ -5,15 +5,6 @@ echo "Current folder - $PWD"
 DIR=$1
 echo "Backup folder $DIR"
 
-#DOP=( $(echo $(docker ps -a | grep ledgeriumcore | awk '{print $1}')) )
-
-#COUNTER=0;
-#for CID in "${DOP[@]}"; do
-#       let COUNTER=COUNTER+1
-#       echo $CID
-#       docker cp $CID:/eth $DIR/eth$COUNTER
-#       docker cp $CID:/priv $DIR/priv$COUNTER
-#done
 echo 'Stopping all containers'
 docker-compose down
 
@@ -21,8 +12,6 @@ echo 'Moving datastore files'
 mkdir -p $DIR/output
 sudo mv tessera-* $DIR/output/
 sudo mv validator-* $DIR/output/
-#rm -rf tessera-*
-#rm -rf validator-*
 
 echo 'Starting all containers'
 docker-compose up -d
